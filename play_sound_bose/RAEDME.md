d4ace4ae17676605373a594945942297e14cd583
Qianhao Yu <qianhaoyu@Qianhaos-MacBook-Pro.local>
2021-02-14 22:35:07 -0600

    Initial commit for all files

---
+++ play_sound_bose/RAEDME.md
@@ -0,0 +1,1 @@
+Mac util that plays pre-recorded sound every 5min that prevents Bose speaker to enter idle (after 20 min) when the main monitor is pluged in. 
---
+++ play_sound_bose/play_sound_rep.py
@@ -0,0 +1,33 @@
+import simpleaudio as sa
+import time
+import subprocess
+import datetime
+import sys
+
+now = str(datetime.datetime.now())
+date = now[:10]
+log = open("logs/log-{}.log".format(date), "a")
+sys.stdout = log
+print('\n==================== {} log ===================='.format(now))
+
+def play_music():
+    filename = 'demo_raw_short.wav'
+    print('Playing music', filename)
+    wave_obj = sa.WaveObject.from_wave_file(filename)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()  # Wait until sound has finished playing
+    print('Finished...')
+
+display = '6CM9100T78' 
+# test if connect to monitor
+# play every 19 min
+
+monitor_info = subprocess.run(['system_profiler', 'SPDisplaysDataType'], stdout=subprocess.PIPE)
+while(True):
+    if display in str(monitor_info.stdout):
+        print('Display is on!')
+        play_music()
+        time.sleep(5*60) # sleep for 19 mins
+    else:
+        print('Display is NOT on!')
+        time.sleep(60)
